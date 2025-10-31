352f5494aa92f40daae65bc6ad2410896a7b5809
GasparDeLeon <gaspardeleon00@gmail.com>
2025-10-31 03:12:24 -0300

    Agregar clase Categoria y ajustes finales de hoy

---
+++ src/main/java/com/obligatorio2025/dominio/Categoria.java
@@ -0,0 +1,24 @@
+package com.obligatorio2025.dominio;
+
+public class Categoria {
+
+    private int id;
+    private String nombre;
+
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
