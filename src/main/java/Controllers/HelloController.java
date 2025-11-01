dc943f7d23bf95027ad6aa37dfa8aa6053aeef83
Matiasdels <matiasdelgado83@gmail.com>
2025-11-01 15:39:19 -0300

    - Implementación de Spring Boot - Estructura modificada (test, resources,controllers

---
+++ src/main/java/Controllers/HelloController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home(){
+        return "Hola desde Spring Boot";
+    }
+}
---
+++ src/test/java/com/obligatorio2025/Obligatorio2025ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obligatorio2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class Obligatorio2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
