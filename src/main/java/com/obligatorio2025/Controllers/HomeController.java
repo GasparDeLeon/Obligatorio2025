1dc41203ecd0247c3b2a415ded9cf0accbb17daf
Matiasdels <matiasdelgado83@gmail.com>
2025-11-13 17:23:47 -0300

    - Botón cerrar sesión en index.html

--- src/main/java/com/obligatorio2025/Controllers/HomeController.java
+++ src/main/java/com/obligatorio2025/Controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.obligatorio2025.Controllers;
 
+import com.obligatorio2025.aplicacion.ServicioAutenticacion;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    private final ServicioAutenticacion servicioAutenticacion;
+
+    public HomeController(ServicioAutenticacion servicioAutenticacion) {
+        this.servicioAutenticacion = servicioAutenticacion;
+    }
+
     @GetMapping({"/", "/index"})
     public String index(HttpSession session, HttpServletResponse response) {
 
@@ -23,5 +31,15 @@ public class HomeController {
 
         System.out.println("Sesión válida. Mostrando index.jsp");
         return "index";
+    }
+
+    @GetMapping("/cerrar-sesion")
+    public String cerrarSesion(HttpSession session) {
+        String sesionId = (String) session.getAttribute("sesionId");
+        if (sesionId != null) {
+            servicioAutenticacion.cerrarSesion(sesionId);
+            session.invalidate();
+        }
+        return "redirect:/login";
     }
 }
